b5cca46328b34a38dfc0dba2427e2d7986cee16b
chip-edwards <50058981+chip-edw@users.noreply.github.com>
2025-01-04 09:49:23 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace AutoTask_Ticket_Manager___Base
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
