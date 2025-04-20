10dc291e9df0de0945f3d90914f914f72bee129b
chip-edwards <50058981+chip-edw@users.noreply.github.com>
2025-04-20 14:31:32 -0500

    Basic Hello World for AdminUI
    Vite setup. Hello world page running

---
+++ AdminUI/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <h1>Hello World from ATTMS AdminUI</h1>
+      <p>We are ready to start building your Admin Portal!</p>
+    </div>
+  );
+}
+
+export default App;
